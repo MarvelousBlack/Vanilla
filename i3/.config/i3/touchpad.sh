f7661ecc1e438e5027e9bfeb333dd5cc99e8777f
Megumi_fox <i@megumifox.com>
2020-11-20 23:20:24 +0800

    ╮(￣▽￣)╭ Setting TPPS/2 IBM TrackPoint speed

--- i3/.config/i3/touchpad.sh
+++ i3/.config/i3/touchpad.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 deviesname="Synaptics TM2668-002"
 notify-send "Disabling  $deviesname..." \ "OFF -  $deviesname disconnected !";
-echo "disable $deviesname..."
+echo "Disable $deviesname..."
 xinput set-prop "$deviesname" 287 1 0
+
+deviesname="TPPS/2 IBM TrackPoint"
+notify-send "Setting $deviesname" "Accel Speed to 1.0000";
+echo "Setting $deviesname Accel Speed to 1.0000"
+xinput set-prop "$deviesname" 321 1 
