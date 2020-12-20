291bd33b07bb3b5bfe71927ab65eab28eeb5fc03
Megumi_fox <i@megumifox.com>
2020-12-20 21:21:25 +0800

    yuyu

--- i3/.config/i3/touchpad.sh
+++ i3/.config/i3/touchpad.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-deviesname="Synaptics TM2668-002"
+#deviesname="Synaptics TM2668-002"
+deviesname="SynPS/2 Synaptics TouchPad"
 notify-send "Disabling  $deviesname..." \ "OFF -  $deviesname disconnected !";
 echo "Disable $deviesname..."
-xinput set-prop "$deviesname" 287 1 0
+xinput set-prop "$deviesname" 295 1 0
 
-deviesname="TPPS/2 IBM TrackPoint"
-notify-send "Setting $deviesname" "Accel Speed to 1.0000";
-echo "Setting $deviesname Accel Speed to 1.0000"
-xinput set-prop "$deviesname" 321 1 
+#deviesname="TPPS/2 IBM TrackPoint"
+#notify-send "Setting $deviesname" "Accel Speed to 1.0000";
+#echo "Setting $deviesname Accel Speed to 1.0000"
+#xinput set-prop "$deviesname" 321 1 
